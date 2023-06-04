7a945a82ab0527fce54ac27a46a70b658abebba9
Zwea Htet <zhtet5@berkeley.edu>
2023-06-03 22:34:03 -0700

    set up backend

---
+++ api/README.md
@@ -0,0 +1,11 @@
+### How to run your api endpoint from your local server
+
+-   First, make sure you have activated your virtual environment. If you see (venv) - the name of your virtual environment within parentheses just before your command prompt, you are good to go.
+
+-   Second, make sure you are in /api directory.
+
+-   Then, execute `python app.py` in command prompt to run your local server.
+
+-   After you finished testing the api endpoint, you may terminate the program and deactivate your virtual environment by typing `deactivate` in command prompt.
+
+-   Later, we could deploy our api endpoint on any cloud (hosting) service provider.
--- popup.js
+++ popup.js
@@ -9,7 +9,37 @@ const onDelete = e => {};
 
 const setBookmarkAttributes =  () => {};
 
-document.addEventListener("DOMContentLoaded", () => {});
+document.addEventListener("DOMContentLoaded", () => {
+    const questionInput = document.getElementById("questionInput");
+    const submitButton = document.getElementById("submitButton");
+    const answerContainer = document.getElementById("answerContainer");
+
+    // Send a question to the backend server and handle the response
+    function askQuestion() {
+        const question = questionInput.value;
+        const apiUrl = "http://your-backend-server.com/api/question"; // Replace with your backend server URL
+
+        fetch(apiUrl, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ question }),
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                // Display the answer in the answerContainer
+                answerContainer.innerText = data.answer;
+            })
+            .catch((error) => {
+                console.error("Error:", error);
+            });
+    }
+
+    // Add event listener to submitButton
+    submitButton.addEventListener("click", askQuestion);
+});
+
 
 // Function for toggle chat widget
 function toggleChatWidget() {
@@ -54,3 +84,4 @@ function sendMessage() {
           messageInput.value = "";
      }
 }
+
