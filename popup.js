58302db09427c81ce3acc3ad8afb18309f1603c1
Wan <wan00582@gmail.com>
2023-06-09 19:39:47 +0630

    Add timestamps to both user and gpt generated message. Add loading message while generating message and placeholder message for generated message. And also added typewriter effect.

--- popup.js
+++ popup.js
@@ -16,14 +16,6 @@ function toggleChatWidget() {
      var chatWidget = document.getElementById("chat-widget");
      var toggleButton = document.getElementById("toggle-button");
 
-     // if (chatWidget.style.display === "none") {
-     //      chatWidget.style.display = "block";
-     //      toggleButton.innerHTML = `<i class="fa-solid fa-x"></i>`;
-     // } else {
-     //      chatWidget.style.display = "none";
-     //      toggleButton.innerHTML = `<i class="fa-solid fa-comment"></i>`;
-     // }
-
      if(!chatWidget.classList.contains('active')){
           chatWidget.classList.add('active');
      }else{
@@ -32,11 +24,45 @@ function toggleChatWidget() {
 }
 
 // Get the chat window to insert user message
-const chatWindow = document.getElementById('chat-window');
+var chatWindow = document.getElementById('chat-window');
+
+// Get time
+function getCurrentTime(){
+     // Get current time
+     const now = new Date();
+     let currentHour = now.getHours();
+     let currentMinute = now.getMinutes();
+     let period = 'am';
+
+     if(currentHour > 12){
+          currentHour -= 12;
+          period = 'pm';
+     }else if(currentHour === 12){
+          period = 'pm';
+     }else if(currentHour = 0){
+          currentHour = 12;
+     }else if(currentHour < 10){
+          // currentHour = '0' + currentHour;
+     }
+
+     // Add leading zero to minute if needed
+     if(currentMinute < 10){
+          currentMinute = '0' + currentMinute;
+     }
+
+     const currentTime = currentHour + ':' + currentMinute + ' ' + period;
+
+     return currentTime;
+}
+
+// Function to scroll the chat container to the bottom
+function scrollToBottom(element) {
+     element.scrollTop = element.scrollHeight;
+}
 
 // Have to add submit event to Form to prevent default form submittion
 const chatSubmitForm = document.getElementById('chatSubmitForm');
-chatSubmitForm.addEventListener('submit', sendMessage)
+chatSubmitForm.addEventListener('submit', sendMessage);
 
 // Sending messages
 function sendMessage(event) {
@@ -49,15 +75,68 @@ function sendMessage(event) {
      var messageInput = document.getElementById("message-input");
      var message = messageInput.value.trim(); // Remove leading/trailing spaces
 
+     // Set the btn disabled to prevent user from sending message while generating message
+     const sendBtn = document.getElementById('send-button');
+
+     // Set the current time to variable from function
+     var currentTime = getCurrentTime();
+
      if (message !== "") {
+          // Set the btn disabled to prevent user from sending message while generating message
+          sendBtn.setAttribute('disabled', true);
+
           // Do something with the message (e.g., send it to the server)
           console.log("Sending message:", message);
           chatWindow.innerHTML += `
                <div class="user-message-container message-container" id="user-message-container">
-                    <p class="message">${message}</p>
-                    <span class="time">9:00 pm</span>
+                    <div class="message">${message}</div>
+                    <span class="time">${currentTime}</span>
+               </div>
+               <div class="gpt-message-container message-container loading" id="gpt-message-container">
+                    <div class="message"><div class="message-generating-loader"></div></div>
                </div>
           `;
+
+          setTimeout(function() {
+               // Generated message
+               var generatedMessage = 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus, quia. Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus, quia. Lorem ipsum dolor sit amet consectetur adipisicing elit.';
+               
+               // Replace to loading message with generated message
+               const loadingMessage = document.querySelector('.loading');
+               loadingMessage.classList.remove('loading');
+               loadingMessage.innerHTML = `
+                    <div class="message currentMessage"></div>
+                    <span class="time">${currentTime}</span>
+               `;
+
+               var i = 0;
+               var speed = 10; /* The speed/duration of the effect in milliseconds */
+               var typingInterval;
+               
+               function startTypingAnimation() {
+                  typingInterval = setInterval(typeWriter, speed);
+               }
+               
+               // Type Writer function
+               function typeWriter() {
+                  if (i < generatedMessage.length) {
+                     document.querySelector(".currentMessage").innerHTML += generatedMessage.charAt(i);
+                     i++;
+                  } else {
+                     clearInterval(typingInterval);
+                     document.querySelector('.currentMessage').classList.remove('currentMessage');
+                     sendBtn.removeAttribute('disabled');
+                  }
+                  scrollToBottom(chatWindow);
+               }
+               
+               // Call the function to start the typing animation
+               startTypingAnimation();
+
+               scrollToBottom(chatWindow);
+          }, 1000);
+
+          scrollToBottom(chatWindow);
 
           // Clear the input field after sending the message
           messageInput.value = "";
